28205a853f8a5da863af9bb290c46d3d453f2d1b
gaveshvimanshna633-a11y <gaveshvimanshna633@gmail.com>
2026-08-16 00:08:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cineflix-/',
+
   plugins: [
     react(),
     tailwindcss()
   ],
+
   server: {
     port: 3000,
     open: true
